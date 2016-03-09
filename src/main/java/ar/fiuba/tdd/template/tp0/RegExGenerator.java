9d488bf9a824981c019da92f4035b09f0e1d6cd9
cesar.buffevant <cbuffevant@gmail.com>
2016-03-09 09:56:52 -0300

    Initial commit

---
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -0,0 +1,24 @@
+package ar.fiuba.tdd.template.tp0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegExGenerator {
+    // TODO: Uncomment this field
+    //private int maxLength;
+
+    //public RegExGenerator(int maxLength) {
+    //    this.maxLength = maxLength;
+    //}
+
+    // TODO: Uncomment parameters
+    public List<String> generate(/*String regEx, int numberOfResults*/) {
+        return new ArrayList<String>() {
+            {
+                add("a");
+                add("b");
+                add("c");
+            }
+        };
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -0,0 +1,67 @@
+package ar.fiuba.tdd.template.tp0;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.assertTrue;
+
+public class RegExGeneratorTest {
+
+    private boolean validate(String regEx, int numberOfResults) {
+        RegExGenerator generator = new RegExGenerator();
+        // TODO: Uncomment parameters
+        List<String> results = generator.generate(/*regEx, numberOfResults*/);
+        // force matching the beginning and the end of the strings
+        Pattern pattern = Pattern.compile("^" + regEx + "$");
+        return results
+                .stream()
+                .reduce(true,
+                    (acc, item) -> {
+                        Matcher matcher = pattern.matcher(item);
+                        return acc && matcher.find();
+                    },
+                    (item1, item2) -> item1 && item2);
+    }
+
+    //TODO: Uncomment these tests
+    /*
+    @Test
+    public void testAnyCharacter() {
+        assertTrue(validate(".", 1));
+    }
+
+    @Test
+    public void testMultipleCharacters() {
+        assertTrue(validate("...", 1));
+    }
+
+    @Test
+    public void testLiteral() {
+        assertTrue(validate("\\@", 1));
+    }
+
+    @Test
+    public void testLiteralDotCharacter() {
+        assertTrue(validate("\\@..", 1));
+    }
+
+    @Test
+    public void testZeroOrOneCharacter() {
+        assertTrue(validate("\\@.h?", 1));
+    }
+
+    @Test
+    public void testCharacterSet() {
+        assertTrue(validate("[abc]", 1));
+    }
+
+    @Test
+    public void testCharacterSetWithQuantifiers() {
+        assertTrue(validate("[abc]+", 1));
+    }
+    */
+    // TODO: Add more tests!!!
+}
