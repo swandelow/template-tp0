2d6478fc1c3b1ba4d65ff70643f72d3064582a5e
sebass <swandelow@despegar.com>
2016-03-16 23:36:02 -0300

    cambio menor. probando build.

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    //private int maxLength;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    private static final int DEFAULT_MAX_LENGTH = 5;
+
+    private int maxLength;
+
+    public RegExGenerator() {
+        this.maxLength = DEFAULT_MAX_LENGTH;
+    }
+
+    public RegExGenerator(int maxLength) {
+        this.maxLength = maxLength;
+    }
 
     // TODO: Uncomment parameters
     public List<String> generate(/*String regEx, int numberOfResults*/) {
