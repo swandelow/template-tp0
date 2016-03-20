125ea238a6bd1bc36f36b57e9d2540a0e87ff418
swandelow <swandelow@despegar.com>
2016-03-20 15:37:04 -0300

    fix checkstyle warning: indentation

--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -16,9 +16,9 @@ public class RegExGeneratorTest {
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
         return results.stream().reduce(true, (acc, item) -> {
-            Matcher matcher = pattern.matcher(item);
-            return acc && matcher.find();
-        }, (item1, item2) -> item1 && item2);
+                Matcher matcher = pattern.matcher(item);
+                return acc && matcher.find();
+            }, (item1, item2) -> item1 && item2);
     }
 
 
